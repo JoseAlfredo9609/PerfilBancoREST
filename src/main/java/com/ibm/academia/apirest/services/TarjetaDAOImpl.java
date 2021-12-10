260ab8741c67e77700d5ae4d492c6ac78c9ae749
JoseAlfredo9609 <josemura.9609@gmail.com>
2021-12-09 18:44:48 -0600

    actualizacion detalles de DAOImpl

--- src/main/java/com/ibm/academia/apirest/services/TarjetaDAOImpl.java
+++ src/main/java/com/ibm/academia/apirest/services/TarjetaDAOImpl.java
@@ -2,6 +2,7 @@ package com.ibm.academia.apirest.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ibm.academia.apirest.models.entities.Tarjeta;
 import com.ibm.academia.apirest.repositories.TarjetaRepository;
@@ -17,16 +18,19 @@ public class TarjetaDAOImpl extends GenericoDAOImpl<Tarjeta, TarjetaRepository> 
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Tarjeta buscarPorNumero(Integer numero) {
 		return repository.buscarPorNumero(numero);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<Tarjeta> findTarjetasBytipoTarjeta(String tipoTarjeta) {
 		return repository.findTarjetasBytipoTarjeta(tipoTarjeta);
 	}
 
 	@Override
+	@Transactional
 	public Tarjeta actualizar(Tarjeta tarjetaEncontrada, Tarjeta tarjeta) {
 		Tarjeta tarjetaActualizada = null;
 		tarjetaEncontrada.setNumero(tarjeta.getNumero());
